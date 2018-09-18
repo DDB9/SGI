d029608b79e35a3340ec667591b1c3328be5bbba
Anne <annevanede@live.nl>
2018-09-18 09:12:53 +0200

    Alternate scripting, not yet finished

---
+++ SGI/Assets/Scripts/Alternate Scripts/Manager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public AudioSource audioDetectiveIntro;
+    public AudioSource audioDetectiveBriefing;
+    public AudioSource audioDean;
+    public AudioSource audioTeacher;
+    public AudioSource audioClerk;
+    public AudioSource audioJanitor;
+
+    public AudioSource menuDetective;
+    public AudioSource menuDean;
+    public AudioSource menuTeacher;
+    public AudioSource menuClerk;
+    public AudioSource menuJanitor;
+    public AudioSource menuAccusation;
+
+    public AudioSource subMenuDean;
+    public AudioSource subMenuTeacher;
+    public AudioSource subMenuClerk;
+    public AudioSource subMenuJanitor;
+    public AudioSource subMenuReturn;
+
+    public AudioSource win;
+    public AudioSource lose;
+
+    GameObject[] objects;
+    Detective detective;
+    Suspect dean;
+    Suspect teacher;
+    Suspect clerk;
+    Suspect janitor;
+    SubMenu accusation;
+
+    int option = 0;
+    bool next, makeAccusation, finished;
+
+    // Use this for initialization
+    void Start () {
+        detective = new Detective(false, menuDetective, audioDetectiveIntro, audioDetectiveBriefing);
+        dean = new Suspect(false, menuDean, audioDean);
+        teacher = new Suspect(true, menuTeacher, audioTeacher);
+        clerk = new Suspect(false, menuClerk, audioClerk);
+        janitor = new Suspect(false, menuJanitor, audioJanitor);
+        accusation = new SubMenu(menuAccusation, new AudioSource[] { subMenuDean, subMenuTeacher, subMenuClerk, subMenuJanitor, subMenuReturn });
+
+        objects = new GameObject[] { detective, dean, teacher, clerk, janitor, accusation };
+        GameLoop();
+    }
+
+    public void GameLoop() {
+        next = detective.PlaySecondAudio();
+        next = detective.MainAction();
+
+        //while (!finished) {
+        //    if (!makeAccusation) {
+        //        //Menu
+        //        while (!Input.GetMouseButtonDown(0)) {
+        //            next = objects[option%6].PlayMenuAudio();
+        //        }
+        //        //Clip
+        //        next = objects[option].MainAction();
+        //        if (option%6 == 5) { makeAccusation = true; }
+
+        //        option = 0;
+        //    } else {
+        //        //SubMenu
+        //        while (!Input.GetMouseButtonDown(0)) {
+        //            next = objects[5].PlaySubMenuAudio(option%5);
+        //        }
+        //        if(option%5 == 4) {
+        //            makeAccusation = false;
+        //        } else if (objects[option%5+1].guilty){
+        //            win.Play();
+        //            finished = true;
+        //        } else {
+        //            lose.Play();
+        //        }
+        //    }
+        //}
+    }
+
+    // Update is called once per frame
+    void Update() {
+        InputHandling();
+    }
+
+    void InputHandling() {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+            option--;
+        } else if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+            option++;
+        }
+    }
+
+
+}
---
+++ SGI/Assets/Scripts/Alternate Scripts/People.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameObject : MonoBehaviour
+{
+    public bool guilty;
+    protected bool isPlaying;
+    protected AudioSource menuAudio;
+
+    public bool PlayMenuAudio() {
+        menuAudio.Play();
+
+        //As long as the audio is playing or the user has not given any input
+        while (!menuAudio.isPlaying) {
+            if(Input.GetMouseButtonDown(0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public virtual bool MainAction() { return true; }
+    public virtual bool PlaySubMenuAudio(int option) { return true; }
+
+}
+
+public class Suspect : GameObject
+{
+    protected AudioSource mainAudio;
+
+    public Suspect(bool Guilty, AudioSource MenuAudio, AudioSource MainAudio) {
+        guilty = Guilty;
+        menuAudio = MenuAudio;
+        mainAudio = MainAudio;
+    }
+
+    public override bool MainAction() {
+        mainAudio.Play();
+
+        //As long as the audio is playing or the user has not given any input
+        while (!mainAudio.isPlaying) {
+            if (Input.GetMouseButtonDown(0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class Detective : GameObject
+{
+    protected AudioSource mainAudio;
+    protected AudioSource secondAudio;
+
+    public Detective(bool Guilty, AudioSource MenuAudio, AudioSource MainAudio, AudioSource SecondAudio) {
+        guilty = Guilty;
+        menuAudio = MenuAudio;
+        mainAudio = MainAudio;
+        secondAudio = SecondAudio;
+    }
+
+    public override bool MainAction() {
+        mainAudio.Play();
+
+        //As long as the audio is playing or the user has not given any input
+        while (!mainAudio.isPlaying) {
+            if (Input.GetMouseButtonDown(0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool PlaySecondAudio() {
+        secondAudio.Play();
+
+        //As long as the audio is playing or the user has not given any input
+        while (!secondAudio.isPlaying) {
+            if (Input.GetMouseButtonDown(0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class SubMenu : GameObject
+{
+    AudioSource[] list;
+
+    public SubMenu(AudioSource MenuAudio, AudioSource[] List) {
+        menuAudio = MenuAudio;
+        list = List;
+    }
+
+    public override bool PlaySubMenuAudio(int option) {
+        list[option].Play();
+
+        //As long as the audio is playing or the user has not given any input
+        while (!list[option].isPlaying) {
+            if (Input.GetMouseButtonDown(0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
