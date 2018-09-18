f6de06acc0edf8ed54810b2bd0bfa978479f4d69
N. Weber <ketoleperouch@gmail.com>
2018-09-18 03:03:42 +0200

    Dynamic Menu
    Nu ipv passief een actief menu systeem

--- SGI/Assets/Scripts/MonoBehaviours/GameController.cs
+++ SGI/Assets/Scripts/MonoBehaviours/GameController.cs
@@ -1,12 +1,174 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public sealed class GameController : MonoBehaviour {
 
+    public enum GameState { Interrogating, Accusing }
+    public GameState gameState;
+
+    public CaseData activeCaseData;           //De active case data: hieruit haalt de GameController de data die per case nodig is
+    public int activeSuspect;                 //De geselecteerde suspect: dus degene die geselecteerd is bij het ondervragen of beschuldigen in het menu. De int is de index in de Suspect array in de Case Data
+    public string activeSuspectName;          //Debug. Alleen voor de inspector.
+
+    public bool canPlayerInput { get; set; }  //True als de speler keuzes kan maken (ondervragen en beschuldigen en zo)
+
+    private AudioSource audioSource;          //De audiosource die alles afspeelt
+
+    private IEnumerator Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //Intro
+        for (int i = 0; i < activeCaseData.introClips.Length; i++)
+        {
+            PlayAtSource(activeCaseData.introClips[i]);
+            //Check of de AudioSource nog aan het afspelen is. Zo niet, dan gaat de Enumerator verder.
+            yield return new WaitUntil(() => !audioSource.isPlaying || Input.GetKeyUp(KeyCode.Space));
+            audioSource.Stop();
+        }
+        PlayAtSource(activeCaseData.suspects[activeSuspect].speakTo);
+        canPlayerInput = true;
+    }
+
+    private void Update()
+    {
+        activeSuspectName = activeCaseData.suspects[activeSuspect].suspectName;
+        if (canPlayerInput)
+        {
+            if (gameState == GameState.Interrogating)
+            {
+                Debug.Log("Suspects ondervragen");
+                //Tijdens het ondervragen van de suspects
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if (activeSuspect <= 0)
+                    {
+                        activeSuspect = activeCaseData.suspects.Length - 1;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].speakTo);
+                    }
+                    else
+                    {
+                        activeSuspect--;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].speakTo);
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    if (activeSuspect >= activeCaseData.suspects.Length - 1)
+                    {
+                        activeSuspect = 0;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].speakTo);
+                    }
+                    else
+                    {
+                        activeSuspect++;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].speakTo);
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Ondervraag een suspect
+                    StartCoroutine(Interrogate(activeSuspect));
+                }
+            }
+            else if (gameState == GameState.Accusing)
+            {
+                Debug.Log("Suspects beschuldigen");
+                //Tijdens het beschuldigen van de suspects
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if (activeSuspect <= 0)
+                    {
+                        activeSuspect = activeCaseData.suspects.Length - 1;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].accusation);
+                    }
+                    else
+                    {
+                        activeSuspect--;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].accusation);
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    if (activeSuspect >= activeCaseData.suspects.Length - 1)
+                    {
+                        activeSuspect = 0;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].accusation);
+                    }
+                    else
+                    {
+                        activeSuspect++;
+                        PlayAtSource(activeCaseData.suspects[activeSuspect].accusation);
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Ondervraag een suspect
+                    StartCoroutine(Accuse(activeSuspect));
+                }
+            }
+        }
+    }
+
+    private IEnumerator Interrogate(int index)
+    {
+        if (activeCaseData.suspects[index].nextMenu)
+        {
+            Debug.Log("Naar Accuse menu");
+            gameState = GameState.Accusing;
+        }
+        canPlayerInput = false;
+        PlayAtSource(activeCaseData.suspects[index].explanation);
+        yield return new WaitUntil(() => !audioSource.isPlaying);
+        audioSource.Stop();
+        canPlayerInput = true;
+    }
+
+    private IEnumerator Accuse(int index)
+    {
+        canPlayerInput = false;
+        if (activeCaseData.suspects[index].nextMenu)
+        {
+            Debug.Log("Naar Interrogate menu");
+            gameState = GameState.Interrogating;
+            canPlayerInput = true;
+            activeSuspect = 0;
+            PlayAtSource(activeCaseData.suspects[0].speakTo);
+            yield break;
+        }
+        //Check of de accusation juist is
+        if (activeCaseData.suspects[index].guilty)
+        {
+            //Juist
+            PlayAtSource(activeCaseData.accuseCorrect);
+            yield return new WaitUntil(() => !audioSource.isPlaying);
+            audioSource.Stop();
+            canPlayerInput = true;
+            //Hier stoppen we de app voor nu.
+            UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
+        else
+        {
+            //Onjuist
+            PlayAtSource(activeCaseData.accuseWrong);
+            yield return new WaitUntil(() => !audioSource.isPlaying);
+            audioSource.Stop();
+            canPlayerInput = true;
+        }
+    }
+
+    private void PlayAtSource(AudioClip clip)
+    {
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+/*
     public enum GameState { Intro, Questioning, Interrogate, Accusation }
-    public GameState gameState = GameState.Intro;
-    public KeyCode accuseKey;   //Keycode in Questioning state om naar Accusation state te gaan
+    public GameState gameState;
     public CaseData caseData;   //Gebruikt manager om de suspects te processen.
+    public KeyCode accuseKey;
 
     public bool waitForInput { get; set; }  //If true, wacht op player keyboard input om het proces te resumeren.
     public bool canAccuse { get; set; }     //Kan de player al accusen of moet ie nog wachten tot de accuseClip afgelopen is?
@@ -76,8 +238,7 @@ public class GameController : MonoBehaviour {
                     }
                     else if (Input.GetKeyDown(accuseKey))
                     {
-                        canAccuse = false;
-                        StartCoroutine(PlayAccuseClip());
+                        PlayAccuseClip();
                     }
                     else
                     {
@@ -88,10 +249,14 @@ public class GameController : MonoBehaviour {
                                 //Accuse suspect hier.
                                 if (caseData.suspects[i].guilty)
                                 {
+                                    m_AudioSource.clip = caseData.accuseCorrect;
+                                    m_AudioSource.Play();
                                     Debug.Log("Goed gedaan zeg.");
                                 }
                                 else
                                 {
+                                    m_AudioSource.clip = caseData.accuseWrong;
+                                    m_AudioSource.Play();
                                     Debug.Log("Probeer opnieuw.");
                                 }
                             }
@@ -102,7 +267,7 @@ public class GameController : MonoBehaviour {
                 {
                     if (!hasAccuseClipPlayed)
                     {
-                        StartCoroutine(PlayAccuseClip());
+                        PlayAccuseClip();
                     }
                 }
             }
@@ -113,8 +278,8 @@ public class GameController : MonoBehaviour {
     {
         m_AudioSource.clip = caseData.menuClip;
         m_AudioSource.Play();
-        yield return new WaitUntil(() => m_AudioSource.isPlaying == false);
         waitForInput = true;
+        yield return new WaitUntil(() => m_AudioSource.isPlaying == false);
         gameState = GameState.Questioning;
     }
 
@@ -129,12 +294,11 @@ public class GameController : MonoBehaviour {
         waitForInput = true;
     }
 
-    private IEnumerator PlayAccuseClip()
+    private void PlayAccuseClip()
     {
         hasAccuseClipPlayed = true;
         m_AudioSource.clip = caseData.whoIsGuiltyClip;
         m_AudioSource.Play();
-        yield return new WaitUntil(() => m_AudioSource.isPlaying == false);
         waitForInput = true;
         canAccuse = true;
     }
@@ -143,5 +307,5 @@ public class GameController : MonoBehaviour {
     private IEnumerator AccuseSuspect(CaseData.Suspect suspect)
     {
         yield break;
-    }
+    }*/
 }
--- SGI/Assets/Scripts/ScriptableObjects/CaseData.cs
+++ SGI/Assets/Scripts/ScriptableObjects/CaseData.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "Case Data")]
-public class CaseData : ScriptableObject {
-
-    public AudioClip[] introClips;          //Alle intro clips die bij de case intro worden afgespeeld
-    [Space]
-    public AudioClip menuClip;              //Clip voor de menu opties
-    public AudioClip whoIsGuiltyClip;       //Clip als je denkt te weten wie de schuldige is
+public sealed class CaseData : ScriptableObject {
 
     [System.Serializable]
-	public class Suspect
+    public sealed class Suspect
     {
-        public string suspectName;          //Utility zonder functie verder. Alleen maar overzichtelijker in Inspector.
-        public AudioClip explanation;       //Audio Clip die wordt gebruikt als de detective de suspect ondervraagt
-        [ForNextVersion][HideInInspector] public AudioClip accusationQuote;   //Audio Clip die wordt gebruikt als de detective de suspect beschuldigt
-        public KeyCode hotkey;             //Tijdens Accusation mode klikt speler op deze Key om de suspect te beschuldigen
-        public bool guilty;                 //Is de suspect de schuldige?
+        [Tooltip("Utility zonder functie verder. Alleen maar overzichtelijker in Inspector")]
+        public string suspectName;
+        [Space]
+        [Tooltip("AudioClip bij \"Speak to ...\" tijdens Question/Interrogation mode")]
+        public AudioClip speakTo;
+        [Tooltip("AudioClip die wordt gebruikt als de detective de suspect ondervraagt")]
+        public AudioClip explanation;
+        [Tooltip("AudioClip bij Accusation mode")]
+        public AudioClip accusation;
+        [Tooltip("Is de suspect de schuldige?")]
+        public bool guilty;
+        [Tooltip("Selecteer dit als de 'Suspect' geen suspect is maar een aanduiding voor een volgend menu.")]
+        public bool nextMenu;   //Als dit true is, dan is de speakTo clip iets wat zegt dat je naar het volgende menu kunt, 
+                                //explanation is het begin van de accusation ("So, who is the murderer?") en accusation dat je naar het vorige menu kunt.
     }
 
-    public Suspect[] suspects;
+    [Header("Intro Setup")]
+    public AudioClip[] introClips;      //Alle introclips op volgorde die worden afgespeeld aan het begin van een beschuldig-session
+
+    [Header("Suspects en Accusation Setup")]
+    public Suspect[] suspects;          //De suspects in de case
+
+    public AudioClip accuseCorrect;     //Clip die je hoort als je de juiste suspect hebt geraden
+    public AudioClip accuseWrong;       //Clip die je hoort als je een verkeerde suspect hebt gekozen
 }
